4f012b69771b58e9d694f28a75b8134655d0d61e
Upendra Raj Bhattarai <upendrarajbhattarai@gmail.com>
2025-05-21 06:44:35 -0400

    removing hello world and square functions

--- code/example_Rcode.R
+++ code/example_Rcode.R
@@ -1,11 +1,6 @@
 # An excercise in R
 
 
-# Print Hello World
-myString <- "Hello, World!"
-
-print (myString)
-
 
 # Monte Carlo Pi
 montecarloPi <- function(trials) {
@@ -38,11 +33,7 @@ est.pi <- function(n){
 }
 
 
-# Square function
-square_it <- function(x){
-  sq <- x*x
-  return(sq)
-}
+
 
 
 #Anscombe's quartet
