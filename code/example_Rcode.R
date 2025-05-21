4192382af2959ce7503b2967a2f3536e70f6f1bc
Upendra Raj Bhattarai <upendrarajbhattarai@gmail.com>
2025-05-21 07:08:50 -0400

    Update example_Rcode.R

--- code/example_Rcode.R
+++ code/example_Rcode.R
@@ -38,21 +38,14 @@ est.pi <- function(n){
 }
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 4f012b6 (removing hello world and square functions)
+
 # Square function
 square_it <- function(x){
   sq <- x*x
   return(sq)
 }
 
-<<<<<<< HEAD
->>>>>>> parent of 4f012b6 (removing hello world and square functions)
-=======
->>>>>>> parent of 4f012b6 (removing hello world and square functions)
+
 
 #Anscombe's quartet
 library(Tmisc)
